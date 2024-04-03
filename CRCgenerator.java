ee616a4f6873638bf7197ca55ae1846c64049739
MarT1AZ <87897542+MarT1AZ@users.noreply.github.com>
2024-04-03 17:18:24 +0700

    update CRCtester and CRCgenerator
    CRCtester
    -add trial function
    -allow randomized trial or customized trail
    -add comments
    CRCgenerator
    -add comments

--- CRCgenerator.java
+++ CRCgenerator.java
@@ -2,40 +2,6 @@ import java.util.Arrays;
 
 public class CRCgenerator {
 
-    public static enum CRC_TYPE {CRC_4,CRC_8,CRC_16,RVS_CRC_16,CRC_24,CRC_32};
-    public static boolean[] CRC4 = new boolean[]{true,false,false,true,true};
-    public static boolean[] CRC8 = new boolean[]{true,true};
-    public static boolean[] CRC16 = new boolean[]{true,true};
-    public static boolean[] RVS_CRC16 = new boolean[]{true,true};
-    public static boolean[] CRC24 = new boolean[]{true,true};
-    public static boolean[] CRC32 = new boolean[]{true,true};
-    
-    public static boolean[] getCRC(CRC_TYPE CT){
-        boolean[] CRC = new boolean[0];
-        switch (CT) {
-            case CRC_4:
-                CRC = CRC4;
-                break;
-            case CRC_8:
-            CRC = CRC8;
-                break;
-            case CRC_16:
-            CRC = CRC16;
-                break;
-            case RVS_CRC_16:
-            CRC = RVS_CRC16;
-                break;
-            case CRC_24:
-            CRC = CRC24;
-                break;
-            case CRC_32:
-            CRC =  CRC32;
-                break;
-        
-        }
-        return CRC;
-    }
-
     public static boolean XOR(boolean B1,boolean B2){
         return ((B1 && !B2) || (!B1 && B2));
     }
@@ -67,15 +33,15 @@ public class CRCgenerator {
         return output;
     }
     
-    public static boolean[] generateCRCcodeWord(boolean[] dataWord,CRC_TYPE CRC){
+    public static boolean[] generateCodeWord(boolean[] dataWord,boolean[] CRC){
         
-        boolean[] divisor = getCRC(CRC);
+        boolean[] divisor = CRC;
 
         int windowEndIndex = divisor.length - 1;
 
-        boolean[] dividendWindow = new boolean[divisor.length];
-        // initialize dividendWindow
 
+        // initialize dividendWindow
+        boolean[] dividendWindow = new boolean[divisor.length];
         for(int i = 0;i < divisor.length;i++){
             if(i < dataWord.length){
                 dividendWindow[i] = dataWord[i]; //bit from dataword
@@ -83,9 +49,9 @@ public class CRCgenerator {
                 dividendWindow[i] = false; // extra 0
             }
         }
+        // initialize dividendWindow
 
-    
-
+        // perform modulo-2 division using XOR operation
         while(windowEndIndex < dataWord.length + divisor.length - 1){
 
             if(windowEndIndex < dataWord.length){
@@ -102,23 +68,36 @@ public class CRCgenerator {
             
             windowEndIndex += 1;
         }
+        // perform modulo-2 division using XOR operation
 
-        return Arrays.copyOfRange(dividendWindow, 0, dividendWindow.length - 1);
+        // dividendWindow is larger than CRC but contain CRC so use the code below to extract CRC
+        boolean[] resultCRC = Arrays.copyOfRange(dividendWindow, 0, dividendWindow.length - 1);
+
+        //merge dataWord with result CRC to get codeWord
+        boolean[] codeWord = new boolean[dataWord.length + resultCRC.length];
+        for(int  i =0 ;i < dataWord.length;i++){
+            codeWord[i] = dataWord[i];
+        }
+        for(int  i =0 ;i < resultCRC.length;i++){
+            codeWord[i + dataWord.length] = resultCRC[i];
+        }
+        return codeWord;
     }
 
-    public static boolean[] calculateSyndrome(boolean[] codeWord,CRC_TYPE CRC){
+    public static boolean[] calculateSyndrome(boolean[] codeWord,boolean[] CRC){
 
-        boolean[] divisor = getCRC(CRC);
+        boolean[] divisor = CRC;
 
         int windowEndIndex = divisor.length - 1;
 
-        boolean[] dividendWindow = new boolean[divisor.length];
         // initialize dividendWindow
-
+        boolean[] dividendWindow = new boolean[divisor.length];
         for(int i = 0;i < divisor.length;i++){
             dividendWindow[i] = codeWord[i];
         }
+        // initialize dividendWindow
 
+        // perform modulo-2 division using XOR operation
         while(windowEndIndex < codeWord.length){
 
             dividendWindow[divisor.length - 1] = codeWord[windowEndIndex];
@@ -132,7 +111,9 @@ public class CRCgenerator {
 
             windowEndIndex += 1;
         }
+        // perform modulo-2 division using XOR operation
 
+        // dividendWindow is larger than CRC but contain CRC so use the code below to extract CRC and return
         return Arrays.copyOfRange(dividendWindow, 0, dividendWindow.length - 1);
     }
 
--- CRCtester.java
+++ CRCtester.java
@@ -3,54 +3,292 @@ import java.util.Random;
 
 public class CRCtester{
 
-    public static boolean[] generateDataWord(int maxWordSize){
-        Random rand = new Random();
-        int size = maxWordSize; // might edit
-        boolean[] dataWord = new boolean[size];
-        for(int i = 0;i < size;i++){
-            dataWord[i] = (rand.nextInt(2) == 1?true:false);
+    public static enum CRC_TYPE {CRC_4,CRC_8,CRC_16,CRC_24,CRC_32};
+
+    
+    public static boolean[] getCRC(CRC_TYPE CT){
+        // return CRC based on CRC_TYPE
+        boolean[] CRC = new boolean[0];
+        switch (CT) {
+            case CRC_4:
+                CRC = new boolean[5];
+                CRC[0] = true;
+                CRC[1] = true;
+                CRC[2] = true;
+                CRC[3] = true;
+                CRC[4] = true;
+                break;
+            case CRC_8:
+                CRC = new boolean[9];
+                CRC[0] = true;
+                CRC[2] = true;
+                CRC[4] = true;
+                CRC[6] = true;
+                CRC[8] = true;
+                break;
+            case CRC_16:
+                CRC = new boolean[17];
+                CRC[0] = true;
+                CRC[2] = true;
+                CRC[15] = true;
+                CRC[16] = true;
+                break;
+            case CRC_24:
+                CRC = new boolean[25];
+                CRC[0] = true;
+                CRC[8] = true;
+                CRC[12] = true;
+                CRC[14] = true;
+                CRC[23] = true;
+                CRC[24] = true;
+                break;
+            case CRC_32:
+                CRC = new boolean[33];
+                CRC[0] = true;
+                CRC[1] = true;
+                CRC[2] = true;
+                CRC[4] = true;
+                CRC[5] = true;
+                CRC[7] = true;
+                CRC[8] = true;
+                CRC[10] = true;
+                CRC[11] = true;
+                CRC[12] = true;
+                CRC[16] = true;
+                CRC[22] = true;
+                CRC[23] = true;
+                CRC[26] = true;
+                CRC[32] = true;
+                break;
+        
         }
-        return dataWord;
+        return CRC;
     }
-    public static void main(String args[]){
 
-        // boolean[] data = generateDataWord(24);
-        // for(int i = 0;i < data.length;i++){
+    // public static boolean[] generateDataWord(int maxWordSize){
+    //     Random rand = new Random();
+    //     int size = maxWordSize; // might edit
+    //     boolean[] dataWord = new boolean[size];
+    //     for(int i = 0;i < size;i++){
+    //         dataWord[i] = (rand.nextInt(2) == 1?true:false);
+    //     }
+    //     return dataWord;
+    // }
 
-        //     System.out.print((data[i]?"1":"0") + " ");
-        // }
+    public static void customTrailCRC(int trialNumber,CRC_TYPE CRCTYPE,boolean[] dataWord,boolean[] isErrorArray){
+        // helper method to generate customize CRC trial
+        // 
+        // user can directly input dataword and the location of errors and CRC type
+        //     
+    
+        trialCRC(trialNumber,CRCTYPE,false,false,false,dataWord,-1,isErrorArray);
+    }
 
-        boolean[] data = new boolean[]{true,false,true,true,false,false,true,true};
-        for(int i = 0;i < data.length;i++){
-            System.out.print((data[i]?"1":"0") + " ");
+    public static void RandomTrailCRC(int trialNumber,CRC_TYPE CRCTYPE,int dataWordLength){
+        // helper method to generate randomized CRC trial
+        // 
+        // user can randomize dataword, dataword length and the location of errors but the CRC type is selected by the user
+        // 
+        trialCRC(trialNumber,CRCTYPE,true,true,false,null,dataWordLength,null);
+    }
+
+    public static void trialCRC(int trialNumber,
+                                CRC_TYPE CRCTYPE,
+                                boolean randomizedDataword,
+                                boolean randomizedError,
+                                boolean isErrorLongerThanCRClength,
+                                boolean[] dataWord,
+                                int dataWordLength,
+                                boolean[] isErrorArray){
+
+        // core method for showing the CRC encoding and decoding output
+
+        
+
+        System.out.print("Trial number : " + trialNumber + "\n\n");
+
+        Random rand = new Random();
+        
+        if(randomizedDataword){
+            dataWord = new boolean[dataWordLength];
+            for(int i = 0;i < dataWordLength;i++){
+                dataWord[i] = (rand.nextInt(2) % 2 == 0);
+            }
         }
-        System.out.println("\n\n");
-        boolean[] output = CRCgenerator.generateCRCcodeWord(data,CRCgenerator.CRC_TYPE.CRC_4);
-        for(int i = 0;i < output.length;i++){
-            System.out.print((output[i]?"1":"0") + " ");
-        }
+        
 
-        boolean[] codeWord = new boolean[data.length + output.length];
-        for(int  i =0 ;i < data.length;i++){
-            codeWord[i] = data[i];
+        System.out.print("dataWord : ");
+        for(int i = 0;i < dataWord.length;i++){
+            System.out.print((dataWord[i]?"1":"0") + " ");
         }
-        for(int  i =0 ;i < output.length;i++){
-            codeWord[i + data.length] = output[i];
+        System.out.print(" length of  : " + dataWord.length);
+        System.out.print("\n\n");
+
+        boolean[] CRC = getCRC(CRCTYPE);
+        System.out.print("CRC used : ");
+        for(int i = 0;i < CRC.length;i++){
+            System.out.print((CRC[i]?"1":"0") + " ");
         }
+        System.out.print(" CRC used : " + CRCTYPE);
+        System.out.print("\n\n");
+
+        System.out.print("codeWord : ");
+        boolean[] codeWord = CRCgenerator.generateCodeWord(dataWord,CRC);
+        for(int i = 0;i < codeWord.length;i++){
+            System.out.print((codeWord[i]?"1":"0") + " ");
+        }
+        System.out.print(" length of  : " + codeWord.length);
+        System.out.print("\n\n");
+
+        int burstErrorLength = 0;
+        int additionalBurstLength = 0;
+        int startErrorIndex = 0;
+        int endErrorIndex = 0;
+        // boolean[] isError
+        if(randomizedError){
+            burstErrorLength = rand.nextInt(CRC.length - 1) + 1;
+            additionalBurstLength = 0;
+            if(isErrorLongerThanCRClength){
+                additionalBurstLength = rand.nextInt(codeWord.length - burstErrorLength);
+            }
+            burstErrorLength += additionalBurstLength;
 
 
-        System.out.println("\n\n");
-        boolean[] syndrome = CRCgenerator.calculateSyndrome(codeWord,CRCgenerator.CRC_TYPE.CRC_4);
+
+            startErrorIndex = rand.nextInt(codeWord.length - burstErrorLength);
+            endErrorIndex = startErrorIndex + burstErrorLength - 1;
+            
+            
+            isErrorArray = new boolean[codeWord.length];
+            isErrorArray[startErrorIndex] = true;
+            codeWord[startErrorIndex] = !codeWord[startErrorIndex];
+            if(startErrorIndex != endErrorIndex){
+                isErrorArray[endErrorIndex] = true;
+                codeWord[endErrorIndex] = !codeWord[endErrorIndex];    
+            }
+            
+            for(int i = startErrorIndex + 1;i < endErrorIndex;i++){
+                if(rand.nextInt(2) % 2 == 1){
+                    isErrorArray[i] = true;
+                    codeWord[i] = !codeWord[i];
+                }
+            }
+        }else{
+            for(int i = 0;i < isErrorArray.length;i++){
+                if(isErrorArray[i]){
+                    startErrorIndex = i;
+                    break;
+                }
+            }
+            for(int i = isErrorArray.length - 1;i >= 0;i--){
+                if(isErrorArray[i]){
+                    endErrorIndex = i;
+                    break;
+                }
+            }
+            burstErrorLength = endErrorIndex - startErrorIndex + 1;
+        }
+        
+
+        System.out.print("applying error of length " + burstErrorLength + "\n\n");
+        System.out.print("start : " + startErrorIndex + " end : " + endErrorIndex);
+        System.out.print("\n\n");
+
+        System.out.print("codeWord : ");
+        for(int i = 0;i < codeWord.length;i++){
+            System.out.print((codeWord[i]?"1":"0") + " ");
+        }
+        System.out.print("\n\n");
+
+        System.out.print("error at : ");
+        for(int i = 0;i < codeWord.length;i++){
+            System.out.print(isErrorArray[i]?"X ":"  ");
+        }
+        System.out.print("\n\n");
+
+        System.out.print("syndrome : ");
+        boolean[] syndrome = CRCgenerator.calculateSyndrome(codeWord,CRC);
         for(int i = 0;i < syndrome.length;i++){
             System.out.print((syndrome[i]?"1":"0") + " ");
         }
+        System.out.print("\n\n");
 
+        boolean isErrorDetected = false;
+        for(int i = 0;i < syndrome.length;i++){
+            if(syndrome[i]){
+                isErrorDetected = true;
+                break;
+            }
+        }
 
-
+        if(isErrorDetected){
+            System.out.print("error detected\n\n");
+        }else{
+            System.out.print("no error detected\n\n");
+        }
 
 
     }
 
+    public static boolean[] toBooleanArray(String s){
+        // use for to transform customized dataword string into boolean array
+        boolean[] res = new boolean[s.length()];
+        for(int i = 0 ;i < s.length();i++){
+            res[i] = s.charAt(i) == '1';
+        }
+        return res;
+    }
+
+    public static void main(String args[]){
+        
+        int trialNumber = 1;
+        String seperator = "=======================================================================\n\n";
+        System.out.print("\n\n");
+        for(int i = 0; i < 2;i++){ // CRC4 with dataword length of 4 (2 cases)
+            RandomTrailCRC(trialNumber,CRC_TYPE.CRC_4, 4);
+            trialNumber++;
+            System.out.print(seperator);
+        }
+
+        for(int i = 0; i < 2;i++){ // CRC8 with dataword length of 6 (2 cases)
+            RandomTrailCRC(trialNumber,CRC_TYPE.CRC_8, 6);
+            trialNumber++;
+            System.out.print(seperator);
+        }
+
+        for(int i = 0; i < 2;i++){ // CRC16 with dataword length of 8 (2 cases)
+            RandomTrailCRC(trialNumber,CRC_TYPE.CRC_16, 8);
+            trialNumber++;
+            System.out.print(seperator);
+        }
+
+        for(int i = 0; i < 2;i++){ // CRC4 with dataword length of 6 (2 cases)
+            RandomTrailCRC(trialNumber,CRC_TYPE.CRC_24, 6);
+            trialNumber++;
+            System.out.print(seperator);
+        }
+
+        for(int i = 0; i < 2;i++){ // CRC32 with dataword length of 4 (2 cases)
+            RandomTrailCRC(trialNumber,CRC_TYPE.CRC_32, 4);
+            trialNumber++;
+            System.out.print(seperator);
+        }
+
+
+        // errorIndexString '1' indicate codeword error position 0 to codeword.length - 1 (left to right)
+        // show cases that fail to detect error
+        String dataWord1 = "10110011";
+        String errorIndexString1 = "010100101000"; // the length is dataWord.length + CRC_4.length - 1
+        System.out.print("case where CRC fail to detect error\n\n");
+        customTrailCRC(trialNumber,CRC_TYPE.CRC_4, toBooleanArray(dataWord1), toBooleanArray(errorIndexString1));
+        trialNumber++;
+        System.out.print(seperator);
+        String dataWord2 = "10110011";
+        String errorIndexString2 = "0100000000010000"; // the length is dataWord.length + CRC_8.length - 1
+        System.out.print("case where CRC fail to detect error\n\n");
+        customTrailCRC(trialNumber,CRC_TYPE.CRC_8, toBooleanArray(dataWord2), toBooleanArray(errorIndexString2));
+
+    }
 
     
 }
