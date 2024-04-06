2ad6a664d18f3fbc92555a7179495c93af72b996
notesuppakorn <107537485+notesuppakorn@users.noreply.github.com>
2024-04-06 14:38:42 +0700

    Update Checksumtester.java

--- Checksumtester.java
+++ Checksumtester.java
@@ -1,7 +1,5 @@
-import java.io.IOException;
-
 public class Checksumtester {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) {
        String text =  ChecksumGenerator.Checksum_gen("File1.txt");
        System.out.println(text);
     }
