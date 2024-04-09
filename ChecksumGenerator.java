fd33fe2331906b10f1c8a6e8b8d4dea5c960338d
notesuppakorn <107537485+notesuppakorn@users.noreply.github.com>
2024-04-10 00:19:20 +0700

    EDITED ChecksumGen and tester

--- ChecksumGenerator.java
+++ ChecksumGenerator.java
@@ -8,44 +8,32 @@ public class ChecksumGenerator {
 		StringBuilder result = new StringBuilder();
 		for(int i = maxLength-1 ; i>=0 ; i--) {
 	    	 int sum = 0;
-//	    	 System.out.println(">> Index: " + i + " <<");
 	    	 int bit1 = (binary1.charAt(i) - '0');
 	    	 int bit2 = (binary2.charAt(i) - '0');
 	    	 sum = sum + bit1 + bit2 + carry;
 	    	 carry = sum/2;
 	    	 result.insert(0, sum%2);
-//	    	 System.out.println(" [1st term]: " + bit1);
-//	    	 System.out.println(" [2nd term]: " + bit2);
-//	    	 System.out.println(" [Old Carry]: "+ carry);
-//	    	 System.out.println("  --> Sum: " + sum);
-//	    	 System.out.println("      Sum % 2: " + sum%2 + " | " +  "Carry: " + carry);
-//	    	 System.out.println("      !! Add " + sum%2 + " to Result !!");
-//	    	 System.out.println("      --> ** RESULT: " + result +" **");
-//	    	 System.out.println();
+
 	     } 
 		 if(carry == 1) {
-	    	 StringBuilder buffer;
-//		     System.out.println("!!!!!! Number of bits exceeds 8 !!!!!!");		     
+	    	 StringBuilder buffer;     
 			 String carry8bit = eightBit(Integer.toString(carry));
 			 carry = 0;
 			 buffer = calculate(carry, maxLength, result.toString(), carry8bit);
 			 result = buffer;
-//	         System.out.println("!!!!!!!!!!!!!!!!!");
+
 	     }
 		return result;
 	}
 	
 	public static String binaryAdder(String binary1, String binary2) {
-//		System.out.println("\n*** Welcome to binaryAdder ***");
-		System.out.println("- Old sum: " + binary1);
-		System.out.println("- Current binary:" + binary2 + "\n");
+
 		 StringBuilder result = new StringBuilder();
 	     int carry = 0;
 	     //The instruction assume that the length of every binary bit us 8 bits
 	     int maxLength = binary1.length();
-	     result = calculate(carry, maxLength, binary1, binary2);
-	     System.out.println("Partial Sum:" +" | Carry: " + carry + " | " + result);
-//		 System.out.println("*** QUIT binaryAdder ***\n");    
+	     result = calculate(carry, maxLength, binary1, binary2); 
+
 	     return result.toString();
 
 	}
@@ -84,7 +72,6 @@ public class ChecksumGenerator {
 			scan.close();
 			
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return text;	
@@ -95,7 +82,7 @@ public class ChecksumGenerator {
     	System.out.println("-- Welcome to Checksum Generator function --");
     	
         /* START of Read Text File */
-    	String text =readFile(fileName);
+    	String text = readFile(fileName);
 		/* END of Read Text File */
 		
 		/* START of Transform Character to Binary string */
@@ -119,11 +106,7 @@ public class ChecksumGenerator {
 		 // Perform binary addition for each remaining binary string
 		System.out.println();
         for (int i = 1; i < binaryStrings.size(); i++) {
-        	/* N terms : N-1 times */
-    		System.out.println("======= Times: " + i + " =======");
             sum = binaryAdder(sum, binaryStrings.get(i));
-            System.out.println("Updated Sum: " + sum);
-            System.out.println("==== End 0f Times: " + i + " ====\n");
         }       
         System.out.println("*******************************");
         System.out.println("Text: " + text);
@@ -136,43 +119,32 @@ public class ChecksumGenerator {
     /**/
     
     /* @CHECKER PART */
-	public static boolean Checksum_check(String filename, String checksum_value) {
+	public static int Checksum_check(String filename, String checksum_value) {
 		ArrayList<String> binaryStrings = new ArrayList<String>();
-    	System.out.println("-- Welcome to Checksum Generator function --");
+    	System.out.println("-- Welcome to Checksum Checker function --");
     	
         /* START of Read Text File */
-    	String text =readFile(filename);
+    	String text = readFile(filename);
 		/* END of Read Text File */
-    	
-    	/* START of Transform Character to Binary string */
+
 		String binary ="";
 		for(int i=0 ; i< text.length()  ; i++) {
 			binary = Integer.toBinaryString(text.charAt(i));
 			if(binary.length() != 8){
 			    binary = eightBit(binary);
 			}
-			if(text.charAt(i) == ' ') {
-				System.out.println("\n" + "SPACE" + " : " + binary);
-			}
-			else {
-				System.out.print(text.charAt(i) + ": " + binary + " | ");
-			}
 			binaryStrings.add(binary);
 		}
-		/* END of Transform Character to Binary string */
-		
-		binaryStrings.add(checksum_value); // Add checksum
-		
+
 		String sum = binaryStrings.get(0);
+		for (int i = 1; i < binaryStrings.size(); i++) {
+            sum = binaryAdder(sum, binaryStrings.get(i));
+        } 
+		sum = binaryAdder(sum, checksum_value);
+		// String sum = binaryStrings.get(0);
 		 // Perform binary addition for each remaining binary string
 		System.out.println();
-       for (int i = 1; i < binaryStrings.size(); i++) {
-       	/* N terms : N-1 times */
-   		System.out.println("======= Times: " + i + " =======");
-           sum = binaryAdder(sum, binaryStrings.get(i));
-           System.out.println("Updated Sum: " + sum);
-           System.out.println("==== End 0f Times: " + i + " ====\n");
-       }       
+   
        System.out.println("**************************************************");
        System.out.println("* Text: " + text);
        System.out.println("* Sum: " + sum);
@@ -180,9 +152,9 @@ public class ChecksumGenerator {
        System.out.println("**************************************************");
 
        if(generateComplement(sum).equals("00000000")) {
-    	   return true;
+    	   return 0;
        }
-       return false;		
+       return -1;		
 	}
     /**/
 }
--- Checksumtester.java
+++ Checksumtester.java
@@ -4,26 +4,73 @@ public class Checksumtester {
     public static void main(String[] args) {
 //    	System.out.println("Hello world");
     	System.out.println("|---------------- @ GENERATOR ---------------|");
-    	ArrayList<String> codeword =  ChecksumGenerator.Checksum_gen("File1.txt");
+    	ArrayList<String> codeword =  ChecksumGenerator.Checksum_gen("File1 (sent).txt ");
     	System.out.println("|------------- END OF GENERATOR -------------|");
     	
     	System.out.println();
     	
     	String checksum = codeword.get(codeword.size()-1);
     	System.out.println("|---------------- @ CHECKER ---------------|");
-    	boolean validity =  ChecksumGenerator.Checksum_check("File2.txt", checksum);
+    	int validity =  ChecksumGenerator.Checksum_check("File1 (recieve).txt", checksum);
     	
     	System.out.println("|------------- END OF CHECKER -------------|");
     	
     	System.out.println();
     	
     	System.out.println("| | | | | | | | | | | | | | | |");
-    	if(validity) {
+    	if(validity == 0) {
     		System.out.println("The received data is correct.");
     	}
     	else {
     		System.out.println("The received data is incorrect!");
     	}
+    	System.out.println("| | | | | | | | | | | | | | | |\n\n\n");
+
+
+		System.out.println("|---------------- @ GENERATOR ---------------|");
+    	codeword =  ChecksumGenerator.Checksum_gen("File2 (sent).txt");
+    	System.out.println("|------------- END OF GENERATOR -------------|");
+    	
+    	System.out.println();
+    	
+    	checksum = codeword.get(codeword.size()-1);
+    	System.out.println("|---------------- @ CHECKER ---------------|");
+    	validity =  ChecksumGenerator.Checksum_check("File2 (recieve).txt", checksum);
+    	
+    	System.out.println("|------------- END OF CHECKER -------------|");
+    	
+    	System.out.println();
+    	
     	System.out.println("| | | | | | | | | | | | | | | |");
+    	if(validity == 0) {
+    		System.out.println("The received data is correct.");
+    	}
+    	else {
+    		System.out.println("The received data is incorrect!");
+    	}
+    	System.out.println("| | | | | | | | | | | | | | | |\n\n\n");
+
+		System.out.println("|---------------- @ GENERATOR ---------------|");
+    	codeword =  ChecksumGenerator.Checksum_gen("File3 (sent).txt");
+    	System.out.println("|------------- END OF GENERATOR -------------|");
+    	
+    	System.out.println();
+    	
+    	checksum = codeword.get(codeword.size()-1);
+    	System.out.println("|---------------- @ CHECKER ---------------|");
+    	validity =  ChecksumGenerator.Checksum_check("File3 (recieve).txt", checksum);
+    	
+    	System.out.println("|------------- END OF CHECKER -------------|");
+    	
+    	System.out.println();
+    	
+    	System.out.println("| | | | | | | | | | | | | | | |");
+    	if(validity == 0) {
+    		System.out.println("The received data is correct.");
+    	}
+    	else {
+    		System.out.println("The received data is incorrect!");
+    	}
+    	System.out.println("| | | | | | | | | | | | | | | |\n\n\n");
     }
 }
